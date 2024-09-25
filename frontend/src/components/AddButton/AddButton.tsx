197e79a83fbb383d622b60078b8bba0f161588b0
Joaquín Ballara <joaquinballara02@gmail.com>
2024-09-25 20:35:05 -0300

    avance pase de js a react

---
+++ frontend/src/components/AddButton/AddButton.tsx
@@ -0,0 +1,8 @@
+import "./AddButton.css";
+
+const AddButton = () => {
+    return (
+        <button id="addTask" className="button is-small is-link">+</button>
+    );
+}
+export default AddButton;
