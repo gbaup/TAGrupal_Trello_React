c8af69ba8f1dd0565b9ce14421749e7cc6b10925
Quino <joaquinballara02@gmail.com>
2024-10-06 11:57:33 -0300

    asignee and endDate now show up in edit

--- frontend/src/components/TaskContent/TaskContent.tsx
+++ frontend/src/components/TaskContent/TaskContent.tsx
@@ -7,7 +7,7 @@ import { store } from '../Store/store'
 
 export const TaskContent = ({ title, id }: CardType) => {
     const [tasks, setTasks] = useState<TaskType[]>([]);
-    store.subscribe(() => setTasks(store.getState().tasks))
+    store.subscribe(() => setTasks(store.getState().tasks));
     return (
         <div className='card-content scroller'>
             {Array.isArray(tasks) && tasks.filter(task => task.status === id).map(task => <Task key={task.id} id={task.id} title={task.title} description={task.description} assignee={task.assignee} endDate={task.endDate} priority={task.priority} status={task.status} />)}
